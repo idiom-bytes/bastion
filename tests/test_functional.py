8216a79de00985e0de1b46a46e5277f6b6e0c42c
idiom-bytes <idiom-bytes@gmail.com>
2020-09-20 12:45:46 -0700

    Updating functional test so that I can create a pair, and verify that balances/reserves are working as intended.

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,8 +1,223 @@
 from unittest import TestCase
-
 from contracting.client import ContractingClient
-from dex_token import dex_token
-from dex import dex
+
+def tau() :
+    balances = Hash(default_value=0)
+    token_name = Variable()
+    token_symbol = Variable()
+
+    # Cannot set breakpoint in @construct
+    @construct
+    def seed(s_name:str, s_symbol: str, vk: str):
+        # Overloading this to mint tokens
+        token_name.set(s_name)
+        token_symbol.set(s_symbol)
+        balances[vk] = 100
+
+    @export
+    def token_name():
+        return token_name.get()
+
+    @export
+    def token_symbol():
+        return token_symbol.get()
+
+    @export
+    def transfer(amount: float, to: str):
+        assert amount > 0, 'Cannot send negative balances!'
+
+        # why is this caller, and not signer?
+        # if we have a contract calling this, although signed by someone else, this will not work
+        sender = ctx.signer
+
+        assert balances[sender] >= amount, 'Not enough coins to send!'
+
+        balances[sender] -= amount
+        balances[to] += amount
+
+    @export
+    def balance_of(account: str):
+        return balances[account]
+
+    @export
+    def main_balance_of(main_account: str, account: str):
+        return balances[main_account, account]
+
+    @export
+    def allowance(owner: str, spender: str):
+        return balances[owner, spender]
+
+    @export
+    def approve(amount: float, to: str):
+        assert amount > 0, 'Cannot send negative balances!'
+
+        sender = ctx.caller
+        balances[sender, to] += amount
+        return balances[sender, to]
+
+    @export
+    def transfer_from(amount: float, to: str, main_account: str):
+        assert amount > 0, 'Cannot send negative balances!'
+
+        # why is this caller, and not signer?
+        # if we have a contract calling this, although signed by someone else, this will not work
+        sender = ctx.caller
+
+        # Why [main_acount,sender] & [sender] -= amount... shouldn't it be?
+        # assert main_account == ctx.signer
+        # balances[main_account] -= amount
+        # balances[to] += amount
+        assert balances[main_account, sender] >= amount, 'Not enough Tau approved to send! You have {} and are trying to spend {}'.format(balances[main_account, sender], amount)
+        assert balances[main_account] >= amount, 'Not enough coins to send!'
+
+        balances[main_account, sender] -= amount
+        balances[main_account] -= amount
+
+        balances[to] += amount
+
+def eth() :
+    balances = Hash(default_value=0)
+    token_name = Variable()
+    token_symbol = Variable()
+
+    # Cannot set breakpoint in @construct
+    @construct
+    def seed(s_name:str, s_symbol: str, vk: str):
+        # Overloading this to mint tokens
+        token_name.set(s_name)
+        token_symbol.set(s_symbol)
+        balances[vk] = 100
+
+    @export
+    def token_name():
+        return token_name.get()
+
+    @export
+    def token_symbol():
+        return token_symbol.get()
+
+    @export
+    def transfer(amount: float, to: str):
+        assert amount > 0, 'Cannot send negative balances!'
+
+        sender = ctx.signer
+
+        assert balances[sender] >= amount, 'Not enough coins to send!'
+
+        balances[sender] -= amount
+        balances[to] += amount
+
+    @export
+    def balance_of(account: str):
+        return balances[account]
+
+    @export
+    def main_balance_of(main_account: str, account: str):
+        return balances[main_account, account]
+
+    @export
+    def allowance(owner: str, spender: str):
+        return balances[owner, spender]
+
+    @export
+    def approve(amount: float, to: str):
+        assert amount > 0, 'Cannot send negative balances!'
+
+        sender = ctx.caller
+        balances[sender, to] += amount
+        return balances[sender, to]
+
+    @export
+    def transfer_from(amount: float, to: str, main_account: str):
+        assert amount > 0, 'Cannot send negative balances!'
+
+        sender = ctx.caller
+
+        assert balances[main_account, sender] >= amount, 'Not enough Token approved to send! You have {} and are trying to spend {}'.format(balances[main_account, sender], amount)
+        assert balances[main_account] >= amount, 'Not enough coins to send!'
+
+        balances[main_account, sender] -= amount
+        balances[main_account] -= amount
+
+        balances[to] += amount
+
+def dex() :
+    # Illegal use of a builtin
+    # import time
+    I = importlib
+
+    # Enforceable interface
+    token_interface = [
+        I.Func('transfer', args=('amount', 'to')),
+        # I.Func('balance_of', args=('account')),
+        I.Func('allowance', args=('owner', 'spender')),
+        I.Func('approve', args=('amount', 'to')),
+        I.Func('transfer_from', args=('amount', 'to', 'main_account'))
+    ]
+
+    pairs = Hash()
+
+    # From UniV2Pair.sol
+    def update(tau, token, tau_balance, token_balance, tau_reserve_last, token_reserve_last):
+        # block_ts = time.time()
+        # time_elapsed = block_ts - pairs[tau.name, token.name, 'block_ts_last']
+
+        # if time_elapsed > 0 and tau_reserve_last != 0 and token_reserve_last != 0 :
+        #     TODO - Calculate price_cumulative_last
+        #     Need time.time() to calculate price_cumulative_last
+        #     pairs[tau.token_name(), token.token_name(), 'tau_price_cumulative_last'] = int(pairs[tau.name, token.name, 'token_reserve'] / pairs[tau.name, token.name, 'tau_reserve']) * time_elapsed
+        #     pairs[tau.token_name(), token.token_name(), 'token_price_cumulative_last'] = int(pairs[tau.name, token.name, 'tau_reserve'] / pairs[tau.name, token.name, 'token_reserve']) * time_elapsed
+
+        pairs[tau.token_name(), token.token_name(), 'tau_reserve'] = tau_balance
+        pairs[tau.token_name(), token.token_name(), 'token_reserve'] = token_balance
+        # pairs[tau.token_name(), token.token_name(), 'block_ts_last'] = block_ts
+
+    @construct
+    def seed():
+        pairs['count'] = 0
+
+    # Simple getter
+    @export
+    def get_length_pairs():
+        return pairs['count']
+
+    @export
+    def get_reserves(tau_contract:str, token_contract:str):
+        return pairs[tau_contract, token_contract, 'tau_reserve'], \
+                pairs[tau_contract, token_contract, 'token_reserve']
+
+    @export
+    def create_pair(tau_contract: str, token_contract: str, tau_amount: int, token_amount: int):
+        assert token_amount > 0
+        assert tau_amount > 0
+
+        # Make sure that what is imported is actually a valid token
+        tau = I.import_module(tau_contract)
+        assert I.enforce_interface(tau, token_interface), 'Tau contract does not meet the required interface'
+
+        token = I.import_module(token_contract)
+        assert I.enforce_interface(token, token_interface), 'Token contract does not meet the required interface'
+
+        assert tau_contract != token_contract
+        assert pairs[tau_contract, token_contract] is None, 'Market already exists!'
+
+        # 1 - This contract will own all amounts sent to it
+        tau.transfer(tau_amount, ctx.this)
+        token.transfer(token_amount, ctx.this)
+
+        # I'm assuming registry of [ctx.this,ctx.investor,amount] is done via LP
+        update(
+            tau,
+            token,
+            tau.balance_of(ctx.this),
+            token.balance_of(ctx.this),
+            pairs[tau.token_name(), token.token_name(), 'tau_reserve'],
+            pairs[tau.token_name(), token.token_name(), 'token_reserve']
+        )
+
+        pairs['count'] += 1
+
+        return ctx.caller, ctx.signer, ctx.this
 
 class MyTestCase(TestCase):
 
@@ -11,55 +226,62 @@ class MyTestCase(TestCase):
         self.client.flush()
 
         # Currently mocking Lamdem functionality w/ dex_token vs. dealing with currency.py
-        self.client.submit(dex_token, 'Lamden', constructor_args={
-            'vk': 'actor1',
-            's_symbol': 'TAU'
+        self.client.submit(tau, 'lamden', constructor_args={
+            's_name': 'lamden',
+            's_symbol': 'TAU',
+            'vk': 'sys'
         })
 
-        self.client.submit(dex_token, 'Ethereum', constructor_args={
-            'vk': 'actor1',
-            's_symbol': 'ETH'
-        })
-
-        self.client.submit(dex_token, 'Antiample', constructor_args={
-            'vk': 'actor1',
-            's_symbol': 'XAMP'
+        self.client.submit(eth, 'ethereum', constructor_args={
+            's_name': 'ethereum',
+            's_symbol': 'ETH',
+            'vk': 'sys'
         })
 
         self.client.submit(dex)
 
-    def test_step1_assign_tokens(self):
+    def test_step1_token_interfaces(self):
         # Get 2 token contracts
-        token0 = self.client.get_contract('Lamden')
-        token1 = self.client.get_contract('Ethereum')
+        token0 = self.client.get_contract('lamden')
+        token1 = self.client.get_contract('ethereum')
 
         # get balances
-        self.assertEqual(token0.name, 'Lamden')
-        self.assertEqual(token0.symbol(), 'TAU')
-        self.assertEqual(token0.quick_read('balances', 'actor1'), 100)
+        self.assertEqual(token0.token_name(), 'lamden')
+        self.assertEqual(token0.token_symbol(), 'TAU')
+        self.assertEqual(token0.quick_read('balances', 'sys'), 100)
+        self.assertEqual(token0.balance_of(account='sys'), 100)
 
-        self.assertEqual(token1.name, 'Ethereum')
-        self.assertEqual(token1.symbol(), 'ETH')
-        self.assertEqual(token1.quick_read('balances', 'actor1'), 100)
+        self.assertEqual(token1.token_name(), 'ethereum')
+        self.assertEqual(token1.token_symbol(), 'ETH')
+        self.assertEqual(token1.quick_read('balances', 'sys'), 100)
+        self.assertEqual(token1.balance_of(account = 'sys'), 100)
 
-    def test_step2_create_pair(self):
+    def test_step2_dex_pair(self):
+        token0 = self.client.get_contract('lamden')
+        token1 = self.client.get_contract('ethereum')
         dex = self.client.get_contract('dex')
 
         n_pairs_before = dex.get_length_pairs()
 
-        dex.create_pair(
-            tau_contract = 'Tau',
-            token_contract = 'Ethereum',
+        caller, signer, this = dex.create_pair(
+            tau_contract = 'lamden',
+            token_contract = 'ethereum',
             tau_amount= 50,
             token_amount = 50
         )
-        dex.create_pair(
-            tau_contract = 'Tau',
-            token_contract = 'Antiample',
-            tau_amount= 50,
-            token_amount = 50
+
+        self.assertEqual(token0.balance_of(account='sys'), 50)
+        self.assertEqual(token0.balance_of(account='dex'), 50)
+
+        self.assertEqual(token1.balance_of(account='sys'), 50)
+        self.assertEqual(token1.balance_of(account='dex'), 50)
+
+        tau_reserve, token_reserve = dex.get_reserves(
+            tau_contract = 'lamden',
+            token_contract = 'ethereum'
         )
+        self.assertEqual(tau_reserve, 50)
+        self.assertEqual(token_reserve, 50)
 
         n_pairs_after = dex.get_length_pairs()
-
         assert n_pairs_after > n_pairs_before
--- tests/test_unit_dex.py
+++ tests/test_unit_dex.py
@@ -1,183 +1,13 @@
 from unittest import TestCase
 from contracting.client import ContractingClient
 
-import time
-
 def dex() :
-    I = importlib
-
-    # Enforceable interface
-    token_interface = [
-        I.Func('transfer', args=('amount', 'to')),
-        I.Func('balance_of', args=('account')),
-        I.Func('allowance', args=('owner', 'spender')),
-        I.Func('approve', args=('amount', 'to')),
-        I.Func('transfer_from', args=('amount', 'to', 'main_account')),
-    ]
-
-    Pairs = Hash()
-
-    def get_reserves(tau_name, token_name):
-        return Pairs[tau_name, token_name, 'tau_reserve'], \
-               Pairs[tau_name, token_name, 'token_reserve'], \
-               Pairs[tau_name, token_name, 'block_ts_last']
-
-    def get_price(tau, token, buy_side = False):
-        if buy_side:
-            return Pairs[tau.name, token.name, 'tau_reserve'] / token.balance_of(ctx.this)
-        else:
-            return token.balance_of(ctx.this) / Pairs[tau.name, token.name, 'tau_reserve']
-
-    # Get token modules, validate & return
-    def get_interface(tau_contract, token_contract):
-        assert Pairs[tau_contract, token_contract] is not None, 'Invalid token ID!'
-
-        # Make sure that what is imported is actually a valid token
-        tau = I.import_module(tau_contract)
-        assert I.enforce_interface(tau, token_interface), 'Token contract does not meet the required interface'
-
-        token = I.import_module(token_contract)
-        assert I.enforce_interface(token, token_interface), 'Token contract does not meet the required interface'
-
-        return tau, token
-
-    # Only one token, and
-    # Returns slippage, rates, etc..
-    # X*Y=Z
-    def get_trade_details(tau, token, tau_out, token_out):
-        # Let's calculate slippage
-        # And how much you get out from the trade
-        tau_reserve = Pairs[tau_contract, token_contract, 'tau_reserve']
-        token_reserve = Pairs[tau_contract, token_contract, 'token_reserve']
-
-        lp_total = tau_reserve * token_reserve
-
-        # Calculate new reserve based on what was passed in
-        tau_reserve_new = tau_reserve + tau_in if tau_in > 0 else 0
-        token_reserve_new = token_reserve + token_in if token_in > 0 else 0
-
-        # Calculate remaining reserve
-        tau_reserve_new = lp_total / token_reserve_new if token_in > 0 else tau_reserve_new
-        token_reserve_new = lp_total / tau_reserve_new if tau_in > 0 else token_reserve_new
-
-        # Calculate how much will be removed
-        tau_out = tau_reserve - tau_reserve_new if token_in > 0 else 0
-        token_out = token_reserve - token_reserve_new if tau_in > 0  else 0
-
-        # Finally, calculate the slippage incurred
-        tau_slippage = tau_reserve / tau_reserve_new if token_in > 0 else 0
-        token_slippage = token_reserve / token_reserve_new  if tau_in > 0 else 0
-
-        return tau_out, token_out, tau_slippage, token_slippage
-
-    # From UniV2Pair.sol
-    def update(tau, token, tau_balance, token_balance, tau_reserve_last, token_reserve_last):
-        block_ts = time.time()
-        time_elapsed = block_ts - Pairs[tau.name, token.name, 'block_ts_last']
-
-        if time_elapsed > 0 and tau_reserve_last != 0 and token_reserve_last != 0 :
-            # TODO - Use "Q notation" calculations to make it float-safe
-            # I understand that this is supposed to average price over time...
-            # But it doesn't seem cumulative (i.e summing time-series information)
-            Pairs[tau.name, token.name, 'tau_price_cumulative_last'] = int(Pairs[tau.name, token.name, 'token_reserve'] / Pairs[tau.name, token.name, 'tau_reserve']) * time_elapsed
-            Pairs[tau.name, token.name, 'token_price_cumulative_last'] = int(Pairs[tau.name, token.name, 'tau_reserve'] / Pairs[tau.name, token.name, 'token_reserve']) * time_elapsed
-
-        Pairs[tau.name, token.name, 'tau_reserve'] = tau_balance
-        Pairs[tau.name, token.name, 'token_reserve'] = token_balance
-        Pairs[tau.name, token.name, 'block_ts_last'] = block_ts
-
-        # emit Sync(reserve0, reserve1);
-
-    # .SOL -> Lamden
-    # amount0Out -> tau_out
-    # amount1Out -> token_out
-    # Ported from UniswapV2Pair.sol @ line 159
-    # // this low-level function should be called from a contract which performs important safety checks
-    # function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
-    def swap(tau_contract:str, token_contract: str, tau_out: float, token_out: float, to: str):
-
-        assert not (tau_out > 0 and token_out > 0), 'Two coin Outputs'
-        assert tau_out > 0 or token_out > 0, 'Insufficient Ouput Amount'
-        tau, token = get_interface(tau_contract, token_contract)
-
-        tau_reserve = Pairs[tau.name, token.name, 'tau_reserve']
-        token_reserve = Pairs[tau.name, token.name, 'token_reserve']
-
-        assert tau_reserve > tau_out and token_reserve > token_out, 'UniswapV2: Inssuficient Liquidity'
-
-        # TODO - A2 - Why is this called BEFORE downstream asserts?
-        # TODO - A2 - How is SOL.safe_transfer() != TAU.transfer_from()
-        if (tau_out > 0):
-            tau.transfer_from(ctx.this, to, tau_out)
-
-        if (token_out > 0):
-            token.transfer_from(ctx.this, to, token_out)
-
-        # TODO - B1 - Identify this call from UniV2
-        # if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
-
-        tau_balance = tau.balance_of(this.ctx)
-        token_balance = token.balance_of(this.ctx)
-
-        tau_in = tau_balance - (tau_reserve - tau_out) if tau_balance > tau_reserve - tau_out else 0
-        token_in = token_balance - (token_reserve - token_out) if token_balance > token_reserve - token_out else 0
-
-        assert tau_in > 0 or token_in > 0, 'UniswapV2: Insufficient Input Amount'
-
-        # TODO - A1/A2 - Deconstruct Curve Adjustment Calculation
-        # ... I'm not sure why balances are being multiplied by 1000, then 3...
-        # I'm guessing this has something to do with smoothing the balance curve
-        tau_balance_adjusted = (tau_balance*1000) - (tau_in*3)
-        token_balance_adjusted = (token_balance*1000) - (token_in*3)
-
-        assert tau_balance_adjusted * token_balance_adjusted >= (tau_reserve * token_reserve) * (1000^2), 'UniswapV2: Exception: K'
-
-        # TODO - A1/A2 - Implement update function
-        update(tau_balance, token_balance, tau_reserve, token_reserve)
-
-        # TODO - B2 - Event Emitters?
-        # emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
 
     # Simple getter
     @export
     def get_length_pairs():
         arr = [1,2]
         return len(arr)
-
-    @export
-    def token_swap(tau_contract:str, token_contract:str, tau_in: float, token_in: float):
-        assert tau_in > 0 or token_in > 0, 'Invalid amount!'
-        assert not tau_in > 0 and token_in > 0, 'Swap only accepts one currecy!'
-        assert Pairs[tau_contract, token_contract] is not None, 'Invalid token ID!'
-
-        assert Pairs[tau_contract, token_contract, 'tau_reserve'] > 0
-        assert Pairs[tau_contract, token_contract, 'token_reserve'] > 0
-
-        tau_reserve_new, token_reserve_new, tau_out, token_out, tau_slippage, token_slippage = get_trade_details()
-
-        tau, token = get_interface(tau_contract, token_contract)
-        swap(tau_contract, token_contract, tau_out, token_out)
-
-    @export
-    def create_pair(tau_contract: str, token_contract: str, tau_amount: int, token_amount: int):
-        assert token_amount > 0
-        assert tau_amount > 0
-
-        # Make sure that what is imported is actually a valid token
-        tau = I.import_module(tau_contract)
-        assert I.enforce_interface(tau, token_interface), 'Token contract does not meet the required interface'
-
-        token = I.import_module(token_contract)
-        assert I.enforce_interface(token, token_interface), 'Token contract does not meet the required interface'
-
-        assert token.name != tau.name
-        assert Pairs[token.name] is None, 'Market already exists!'
-
-        # 1 - This contract will own all amounts sent to it
-        token.transfer_from(ctx.sender, ctx.this, token_amount)
-        tau.transfer_from(ctx.sender, ctx.this, tau_amount)
-
-        update(tau.balance_of(ctx.this), token.balance_of(ctx.this), tau_amount, token_amount)
 
 class MyTestCase(TestCase):
 
@@ -187,7 +17,7 @@ class MyTestCase(TestCase):
 
         self.client.submit(dex, 'dex')
 
-    def test_create_pair(self):
+    def test_dex_function(self):
         dex = self.client.get_contract('dex')
 
         self.assertEqual(dex.get_length_pairs(), 2)
